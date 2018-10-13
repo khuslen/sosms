60a85539215d794e8affe987bcfde2b95718032d
Khuslen Baatarsukh <khuslen96@yahoo.com>
2018-10-13 20:55:33 +1000

    added reply function to server, added more req handlers

--- server/server.js
+++ server/server.js
@@ -38,77 +38,84 @@ wss.on("connection", function connection(ws) {
         console.log("RECEIVED", msg);
         switch (msg.req) {
             case "login":
-            login(ws, msg);
-            break;
+                login(ws, msg);
+                break;
             case "getUserInfo":
-            getUserInfo(ws, msg);
-            break;
-            case "confirmSafety":
-            confirmSafety(ws, msg);
-            break;
+                getUserInfo(ws, msg);
+                break;
+            case "safetyBtn":
+                safetyBtn(ws, msg);
+                break;
+            case "locationBtn":
+                locationBtn(ws, msg);
+                break;
             default:
-            break;
+                break;
         }
     });
     try {
-        // ws.send(JSON.stringify({"res": "Connected to server"}));
-        ws.send(JSON.stringify({
-            "res": "incident",
-            "data": {
-                "address": "1 Office Street",
-                "name": "Fire on Level 14",
-                "desc": "An electrical fire has broken out in the Level 14 Server Room at 7:51am AEST this morning.",
-                "severity": "S2",
-                "incident": "true"
-            }
-        }));
+        const data = {
+            address: "1 Office Street",
+            name: "Fire on Level 14",
+            desc: "An electrical fire has broken out in the Level 14 Server Room at 7:51am AEST this morning.",
+            severity: "S2",
+            incident: "true"
+        };
+        reply(ws, "incident", data);
     } catch (err) {
         console.log(err);
     }
 });
 
 function sendUpdates(ws, updates) {
-    ws.send(JSON.stringify({
-        "res": "updates",
-        "data": {
-            "updates": [
-                {
-                    "time": "7:51AM 14/10/2018",
-                    "text": "Incident occurred."
-                },
-                {
-                    "time": "7:53AM 14/10/2018",
-                    "text": "Evacuation commenced."
-                },
-                {
-                    "time": "7:53AM 14/10/2018",
-                    "text": "Emergency services have been contacted."
-                },
-                {
-                    "time": "8:03AM 14/10/2018",
-                    "text": "Emergency services have arrived."
-                }
-            ]
-        }
-    }))
+    const data = {
+        "updates": [
+            {
+                "time": "7:51AM 14/10/2018",
+                "text": "Incident occurred."
+            },
+            {
+                "time": "7:53AM 14/10/2018",
+                "text": "Evacuation commenced."
+            },
+            {
+                "time": "7:53AM 14/10/2018",
+                "text": "Emergency services have been contacted."
+            },
+            {
+                "time": "8:03AM 14/10/2018",
+                "text": "Emergency services have arrived."
+            }
+        ]
+    };
+    reply(ws, "updates", data);
 }
 
-function reply(ws, response) {
-    ws.send(JSON.stringify({"res": response}));
+function reply(ws, res, data) {
+    if (ws) {
+        ws.send(JSON.stringify({
+          res: res,
+          data: data
+        }));
+    }
 }
 
 function login(ws, msg) {
-
+    if (msg.data.hasOwnProperty("userId")) {
+        reply(ws, "error", "Please provide user ID!");
+    } else {
+        ws.userId = msg.data.userId;
+        reply(ws, "login", "Successful login!");
+    }
 }
 
 function getUserInfo(ws, msg) {
-    // console.log(users);
-    reply(ws, users);
+    reply(ws, "getUserInfo", users);
 }
 
-function confirmSafety(ws, msg) {
-    if (msg.reply !== "Y" && msg.reply !== "N") {
-        reply(ws, "Please send a valid response!");
+function safetyBtn(ws, msg) {
+    if (msg.data.safe !== "Y" && msg.data.safe !== "N") {
+        reply(ws, "error", "Please send a valid safety response!");
     } else if (msg.reply === "Y") {
 
     } else if (msg.reply === "N") {
@@ -116,6 +123,12 @@ function confirmSafety(ws, msg) {
     }
 }
 
-function broadcast(msg, senderId) {
-  
+function locationBtn(ws, msg) {
+    if (msg.data.location !== "W" && msg.data.location !== "H") {
+        reply(ws, "error", "Please send a valid location response!");
+    } else if (msg.data.location === "W") {
+
+    } else if (msg.data.location === "H") {
+
+    }
 }
